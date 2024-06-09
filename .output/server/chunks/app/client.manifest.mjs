9f9dd971242c50ab2f01f5da3d82a6eb32c18d56
matusporuben <matus.poruben@gmail.com>
2024-06-09 18:54:28 +0200

    change port

---
+++ .output/server/chunks/app/client.manifest.mjs
@@ -0,0 +1,377 @@
+const client_manifest = {
+  "_Container.VqURBzZB.js": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "Container.VqURBzZB.js",
+    "imports": [
+      "node_modules/nuxt/dist/app/entry.js"
+    ]
+  },
+  "_Sectionhead.03W5N3Ya.js": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "Sectionhead.03W5N3Ya.js",
+    "imports": [
+      "node_modules/nuxt/dist/app/entry.js"
+    ]
+  },
+  "_index.hPcBRl07.js": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "index.hPcBRl07.js"
+  },
+  "_nuxt-link.r47RsC-H.js": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "nuxt-link.r47RsC-H.js",
+    "imports": [
+      "node_modules/nuxt/dist/app/entry.js"
+    ]
+  },
+  "_vue.f36acd1f.Ouc3h9R7.js": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "vue.f36acd1f.Ouc3h9R7.js",
+    "imports": [
+      "node_modules/nuxt/dist/app/entry.js"
+    ]
+  },
+  "assets/img/change-code.gif": {
+    "resourceType": "image",
+    "prefetch": true,
+    "mimeType": "image/gif",
+    "file": "change-code.7gL_Fp8D.gif",
+    "src": "assets/img/change-code.gif"
+  },
+  "assets/img/git-push.png": {
+    "resourceType": "image",
+    "prefetch": true,
+    "mimeType": "image/png",
+    "file": "git-push.Gf_MEvZ8.png",
+    "src": "assets/img/git-push.png"
+  },
+  "assets/img/git-push2.jpg": {
+    "resourceType": "image",
+    "prefetch": true,
+    "mimeType": "image/jpeg",
+    "file": "git-push2.Cu7LrUIJ.jpg",
+    "src": "assets/img/git-push2.jpg"
+  },
+  "assets/img/happy.gif": {
+    "resourceType": "image",
+    "prefetch": true,
+    "mimeType": "image/gif",
+    "file": "happy.O-rRmi8t.gif",
+    "src": "assets/img/happy.gif"
+  },
+  "assets/img/hero.jpg": {
+    "resourceType": "image",
+    "prefetch": true,
+    "mimeType": "image/jpeg",
+    "file": "hero.FbnOsFWO.jpg",
+    "src": "assets/img/hero.jpg"
+  },
+  "assets/img/image1.jpg": {
+    "resourceType": "image",
+    "prefetch": true,
+    "mimeType": "image/jpeg",
+    "file": "image1.iw3t8D0G.jpg",
+    "src": "assets/img/image1.jpg"
+  },
+  "assets/img/login-to-aftp.png": {
+    "resourceType": "image",
+    "prefetch": true,
+    "mimeType": "image/png",
+    "file": "login-to-aftp.yeJnKhQX.png",
+    "src": "assets/img/login-to-aftp.png"
+  },
+  "assets/img/npm-run-build.png": {
+    "resourceType": "image",
+    "prefetch": true,
+    "mimeType": "image/png",
+    "file": "npm-run-build.-AXGTsoV.png",
+    "src": "assets/img/npm-run-build.png"
+  },
+  "assets/img/typing-cat.gif": {
+    "resourceType": "image",
+    "prefetch": true,
+    "mimeType": "image/gif",
+    "file": "typing-cat.7Fdt5BxP.gif",
+    "src": "assets/img/typing-cat.gif"
+  },
+  "assets/img/wow.gif": {
+    "resourceType": "image",
+    "prefetch": true,
+    "mimeType": "image/gif",
+    "file": "wow.IKICddIl.gif",
+    "src": "assets/img/wow.gif"
+  },
+  "assets/img/wtf.gif": {
+    "resourceType": "image",
+    "prefetch": true,
+    "mimeType": "image/gif",
+    "file": "wtf.W4BFAVNF.gif",
+    "src": "assets/img/wtf.gif"
+  },
+  "layouts/landing.vue": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "landing.swpZMlQz.js",
+    "imports": [
+      "_Container.VqURBzZB.js",
+      "node_modules/nuxt/dist/app/entry.js"
+    ],
+    "isDynamicEntry": true,
+    "src": "layouts/landing.vue"
+  },
+  "node_modules/@nuxt/ui-templates/dist/templates/error-404.vue": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "css": [],
+    "file": "error-404.vRFnikm1.js",
+    "imports": [
+      "_nuxt-link.r47RsC-H.js",
+      "_vue.f36acd1f.Ouc3h9R7.js",
+      "node_modules/nuxt/dist/app/entry.js"
+    ],
+    "isDynamicEntry": true,
+    "src": "node_modules/@nuxt/ui-templates/dist/templates/error-404.vue"
+  },
+  "error-404.qFGwA4uS.css": {
+    "file": "error-404.qFGwA4uS.css",
+    "resourceType": "style",
+    "prefetch": true,
+    "preload": true
+  },
+  "node_modules/@nuxt/ui-templates/dist/templates/error-500.vue": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "css": [],
+    "file": "error-500.x-yzMxIA.js",
+    "imports": [
+      "_vue.f36acd1f.Ouc3h9R7.js",
+      "node_modules/nuxt/dist/app/entry.js"
+    ],
+    "isDynamicEntry": true,
+    "src": "node_modules/@nuxt/ui-templates/dist/templates/error-500.vue"
+  },
+  "error-500.V0P2JAtD.css": {
+    "file": "error-500.V0P2JAtD.css",
+    "resourceType": "style",
+    "prefetch": true,
+    "preload": true
+  },
+  "node_modules/nuxt-icon/dist/runtime/Icon.vue": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "css": [],
+    "file": "Icon.R_rPA1XU.js",
+    "imports": [
+      "node_modules/nuxt/dist/app/entry.js",
+      "_index.hPcBRl07.js"
+    ],
+    "isDynamicEntry": true,
+    "src": "node_modules/nuxt-icon/dist/runtime/Icon.vue"
+  },
+  "Icon.2p9d7H8O.css": {
+    "file": "Icon.2p9d7H8O.css",
+    "resourceType": "style",
+    "prefetch": true,
+    "preload": true
+  },
+  "node_modules/nuxt-icon/dist/runtime/IconCSS.vue": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "css": [],
+    "file": "IconCSS.nQAy7BCo.js",
+    "imports": [
+      "node_modules/nuxt/dist/app/entry.js",
+      "_index.hPcBRl07.js"
+    ],
+    "isDynamicEntry": true,
+    "src": "node_modules/nuxt-icon/dist/runtime/IconCSS.vue"
+  },
+  "IconCSS.GdgQB7f8.css": {
+    "file": "IconCSS.GdgQB7f8.css",
+    "resourceType": "style",
+    "prefetch": true,
+    "preload": true
+  },
+  "node_modules/nuxt/dist/app/entry.js": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "dynamicImports": [
+      "layouts/landing.vue",
+      "node_modules/@nuxt/ui-templates/dist/templates/error-404.vue",
+      "node_modules/@nuxt/ui-templates/dist/templates/error-500.vue"
+    ],
+    "file": "entry.8wXXEchf.js",
+    "isEntry": true,
+    "src": "node_modules/nuxt/dist/app/entry.js",
+    "_globalCSS": true
+  },
+  "pages/after.vue": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "assets": [
+      "wow.IKICddIl.gif",
+      "typing-cat.7Fdt5BxP.gif",
+      "git-push2.Cu7LrUIJ.jpg",
+      "happy.O-rRmi8t.gif"
+    ],
+    "file": "after.-jQFLcRu.js",
+    "imports": [
+      "_Sectionhead.03W5N3Ya.js",
+      "_Container.VqURBzZB.js",
+      "node_modules/nuxt/dist/app/entry.js"
+    ],
+    "isDynamicEntry": true,
+    "src": "pages/after.vue"
+  },
+  "wow.IKICddIl.gif": {
+    "file": "wow.IKICddIl.gif",
+    "resourceType": "image",
+    "prefetch": true,
+    "mimeType": "image/gif"
+  },
+  "typing-cat.7Fdt5BxP.gif": {
+    "file": "typing-cat.7Fdt5BxP.gif",
+    "resourceType": "image",
+    "prefetch": true,
+    "mimeType": "image/gif"
+  },
+  "git-push2.Cu7LrUIJ.jpg": {
+    "file": "git-push2.Cu7LrUIJ.jpg",
+    "resourceType": "image",
+    "prefetch": true,
+    "mimeType": "image/jpeg"
+  },
+  "happy.O-rRmi8t.gif": {
+    "file": "happy.O-rRmi8t.gif",
+    "resourceType": "image",
+    "prefetch": true,
+    "mimeType": "image/gif"
+  },
+  "pages/before.vue": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "assets": [
+      "image1.iw3t8D0G.jpg",
+      "change-code.7gL_Fp8D.gif",
+      "git-push.Gf_MEvZ8.png",
+      "npm-run-build.-AXGTsoV.png",
+      "login-to-aftp.yeJnKhQX.png",
+      "wtf.W4BFAVNF.gif"
+    ],
+    "css": [],
+    "file": "before.S2Lf9K49.js",
+    "imports": [
+      "_Sectionhead.03W5N3Ya.js",
+      "node_modules/nuxt/dist/app/entry.js",
+      "_Container.VqURBzZB.js"
+    ],
+    "isDynamicEntry": true,
+    "src": "pages/before.vue"
+  },
+  "before.WvQqEIvz.css": {
+    "file": "before.WvQqEIvz.css",
+    "resourceType": "style",
+    "prefetch": true,
+    "preload": true
+  },
+  "image1.iw3t8D0G.jpg": {
+    "file": "image1.iw3t8D0G.jpg",
+    "resourceType": "image",
+    "prefetch": true,
+    "mimeType": "image/jpeg"
+  },
+  "change-code.7gL_Fp8D.gif": {
+    "file": "change-code.7gL_Fp8D.gif",
+    "resourceType": "image",
+    "prefetch": true,
+    "mimeType": "image/gif"
+  },
+  "git-push.Gf_MEvZ8.png": {
+    "file": "git-push.Gf_MEvZ8.png",
+    "resourceType": "image",
+    "prefetch": true,
+    "mimeType": "image/png"
+  },
+  "npm-run-build.-AXGTsoV.png": {
+    "file": "npm-run-build.-AXGTsoV.png",
+    "resourceType": "image",
+    "prefetch": true,
+    "mimeType": "image/png"
+  },
+  "login-to-aftp.yeJnKhQX.png": {
+    "file": "login-to-aftp.yeJnKhQX.png",
+    "resourceType": "image",
+    "prefetch": true,
+    "mimeType": "image/png"
+  },
+  "wtf.W4BFAVNF.gif": {
+    "file": "wtf.W4BFAVNF.gif",
+    "resourceType": "image",
+    "prefetch": true,
+    "mimeType": "image/gif"
+  },
+  "pages/index.vue": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "assets": [
+      "hero.FbnOsFWO.jpg"
+    ],
+    "css": [],
+    "file": "index.EYgpVid-.js",
+    "imports": [
+      "_nuxt-link.r47RsC-H.js",
+      "node_modules/nuxt/dist/app/entry.js",
+      "_Container.VqURBzZB.js"
+    ],
+    "isDynamicEntry": true,
+    "src": "pages/index.vue"
+  },
+  "index.MJGdeUIk.css": {
+    "file": "index.MJGdeUIk.css",
+    "resourceType": "style",
+    "prefetch": true,
+    "preload": true
+  },
+  "hero.FbnOsFWO.jpg": {
+    "file": "hero.FbnOsFWO.jpg",
+    "resourceType": "image",
+    "prefetch": true,
+    "mimeType": "image/jpeg"
+  }
+};
+
+export { client_manifest as default };
+//# sourceMappingURL=client.manifest.mjs.map
